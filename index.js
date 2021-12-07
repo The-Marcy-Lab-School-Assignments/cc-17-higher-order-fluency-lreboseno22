bf9e9c778b41523d621d390d1a11fd9377b4a20a
lreboseno22 <liamrebo22@gmail.com>
2021-12-07 15:49:27 +0000

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 //write your code here
+
+const animalArray = ['cheetah', 'dog', 'cat', 'dodobird', 'bear', 'dolphin'];
+
+const pluralize = function(array){
+    return array.map(word => `${word}s`);
+};
+console.log(pluralize(animalArray));
+
+const upperCase = function(array){
+    return array.map(word => word[0].toUpperCase() + word.substr(1));
+};
+console.log(upperCase(animalArray));
+
+const longWords = function(array){
+    return array.filter(word => word.length > 3);
+};
+console.log(longWords(animalArray));
+
+const shortWords = function(array){
+    return array.filter(word => word.length < 5);
+};
+console.log(shortWords(animalArray));
+
+const oddLength = function(array){
+    return array.filter(word => word.length % 2 !== 0);
+};
+console.log(oddLength(animalArray));
+
+const longToShort = function(array){
+    return array.sort((a, b) => b.length - a.length);
+};
+console.log(longToShort(animalArray));
+
+const sum = function(array){
+    return array.reduce((a, b) => a + b);
+};
+console.log(sum([22, 15, 1114, 416, 37, 4]));
